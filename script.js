b13b813de48c72ba2a85c836d95cbe9708a6bbce
Jarhead <hdog358@aol.com>
2022-11-03 16:15:34 -0700

    clean up code

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // create 16 x 16 grid with for loop
 const container = document.querySelector('#container');
-//container.style.backgroundColor = 'black';
-// const item = document.createElement('item');
 
 var chosenRows = 16;
 var chosenCols = 16;
+// Creates grid
 function makeGrid(rows,cols){
     for (i = 0; i < (rows * cols); i++){
         item = document.createElement('item');
         item.classList.add('item');
-        //item.textContent = i + 1;
-        //item.style.backgroundColor = 'white';
         item.style.border = '1px solid black';
         container.appendChild(item);
     }
     
 }
+// Calls grid funtion
 makeGrid(chosenRows,chosenCols);
+
+// Changes Grid
 function change(){
     document.getElementById("container").innerHTML = "";
     
@@ -38,23 +38,4 @@ function change(){
     rowLength.style.setProperty('--col', val2);
     const container = document.querySelector('#container');
     makeGrid(val1,val2);
-    // chosenRows2 = 2;
-    // chosenCols2 = 2;
-    // function makeGrid(chosenRows2,chosenCols2);
 }
-// item.addEventListener('mouseenter',(container) => {
-//     item.style.backgroundColor = 'black';
-// } )
-// item.addEventListener('mouseleave',(container) => {
-//     item.style.backgroundColor = 'white';
-// })
-// function makeGrid(rows,cols){
-//     for (i = 0; i < (rows,cols); i++){
-//         let square = document.createElement('div');
-//         square.classList.add('square');
-//         container.appendChild(square);
-//     }
-// }
-// makeGrid(16,16);
-// use dom method to create all the squares
-// add listener with hover
