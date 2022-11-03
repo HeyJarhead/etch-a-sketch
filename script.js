c13fbe8ea196c9621539e153fdaa21ce3b053eb1
Jarhead <hdog358@aol.com>
2022-11-03 13:15:45 -0700

    Add hover

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 // create 16 x 16 grid with for loop
 const container = document.querySelector('#container');
-container.style.backgroundColor = 'black';
+//container.style.backgroundColor = 'black';
+// const item = document.createElement('item');
+
 var chosenRows = 16;
 var chosenCols = 16;
 function makeGrid(rows,cols){
     for (i = 0; i < (rows * cols); i++){
-        let item = document.createElement('item');
+        item = document.createElement('item');
         item.classList.add('item');
         item.textContent = i;
-        item.style.backgroundColor = 'white';
+        //item.style.backgroundColor = 'white';
         item.style.border = '1px solid black';
         container.appendChild(item);
-        console.log(i);
+        
     }
+    
 }
 makeGrid(chosenRows,chosenCols);
+// item.addEventListener('mouseenter',(container) => {
+//     item.style.backgroundColor = 'black';
+// } )
+// item.addEventListener('mouseleave',(container) => {
+//     item.style.backgroundColor = 'white';
+// })
 // function makeGrid(rows,cols){
 //     for (i = 0; i < (rows,cols); i++){
 //         let square = document.createElement('div');
